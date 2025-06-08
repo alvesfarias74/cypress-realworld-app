347e23b70ba6821ca71c49a1c00b92a547bcec41
alvesfarias74 <alvesfarias74@gmail.com>
2025-06-08 09:53:12 -0300

    bug fixes in e2e testse

--- cypress/tests/e2e/mylogin.spec.ts
+++ cypress/tests/e2e/mylogin.spec.ts
@@ -4,6 +4,12 @@ describe("Login com sucesso", () => {
     cy.get("[name='username']").type("Dina20");
     cy.get("[name='password']").type("s3cret");
     cy.get("[type='submit']").click();
+
+    // Verifica se a URL foi alterada para o dashboard
+    cy.url().should("include", "http://localhost:3000/");
+
+    // Verifica se o usuário está logado pelo nome exibido na tela
+    cy.get("[data-test='sidenav-username']").should("contain", "Dina20");
   });
 });
 
@@ -13,25 +19,33 @@ describe("Tentar fazer login com credenciais inválidas", () => {
     cy.get("[name='username']").type("Alves74");
     cy.get("[name='password']").type("s3cret");
     cy.get("[type='submit']").click();
+
+    // Verifica se a mensagem de erro é exibida corretamente
+    cy.get("[role='alert']").should("be.visible").and("contain", "Username or password is invalid");
+
+    // Verifica se a URL não mudou, indicando que o login falhou
+    cy.url().should("eq", "http://localhost:3000/signin");
   });
 });
 
-describe('Registro de novo usuário com sucesso', () => {
-  it('Deve registrar um novo usuário com informações válidas', () => {
+describe("Registro de novo usuário com sucesso", () => {
+  it("Deve registrar um novo usuário com informações válidas", () => {
     cy.visit("http://localhost:3000/");
     cy.get("[href='/signup']").click();
-    cy.get("[name='firstName']").type("Francisco");
-    cy.get("[name='lastName']").type("Alves");
-    cy.get("[name='username']").type("alvesfarias74");
+    cy.get("[name='firstName']").type("Francisca");
+    cy.get("[name='lastName']").type("Marcia");
+    cy.get("[name='username']").type("marcyamart");
     cy.get("[name='password']").type("12345678");
     cy.get("[name='confirmPassword']").type("12345678");
     cy.get(".SignUpForm-submit").click();
     
+    // Verifica se o usuário foi redirecionado para a tela de login após o cadastro
+    cy.url().should("include", "/signin");
   });
 });
 
-describe('Tentar registrar um novo usuário com informações incompletas', () => {
-  it.only('Deve exibir mensagens de erro ao tentar registrar um novo usuário sem preencher todas as informações obrigatórias', () => {
+describe("Tentar registrar um novo usuário com informações incompletas", () => {
+  it("Deve exibir mensagens de erro ao tentar registrar um novo usuário sem preencher todas as informações obrigatórias", () => {
     cy.visit("http://localhost:3000/");
     cy.get("[href='/signup']").click();
     cy.get("[name='firstName']").type("Ester");
@@ -39,6 +53,11 @@ describe('Tentar registrar um novo usuário com informações incompletas', () =
     cy.get("[name='username']").type("esterfarias16");
     cy.get("[name='password']").click();
     cy.get("[name='confirmPassword']").click();
-    cy.get("#password-helper-text");    
+
+    // Verifica se a mensagem de erro do campo password está visível
+    cy.get("#password-helper-text").should("be.visible").and("contain", "Enter your password");
+
+    // Verifica se o botão de envio está desabilitado
+    cy.get(".SignUpForm-submit").should("be.disabled");
   });
 });
--- cypress/tests/e2e/mysendmoney.spec.ts
+++ cypress/tests/e2e/mysendmoney.spec.ts
@@ -1,4 +1,4 @@
-describe("Enviar dinheiro com saldo suficiente", () => {
+/* describe("Enviar dinheiro com saldo suficiente", () => {
   it("Deve enviar dinheiro com sucesso", () => {
     cy.visit("http://localhost:3000/");
     cy.get("[name='username']").type("Reyes.Osinski");
@@ -25,5 +25,47 @@ describe("Enviar dinheiro com saldo insuficiente", () => {
     cy.get("[name='amount']").type(2000);
     cy.get("[placeholder='Add a note']").type("Legal expenses and costs");
     cy.get("[data-test='transaction-create-submit-payment']").click();
+  });
+}); */
+
+describe("Enviar dinheiro com saldo suficiente", () => {
+  it.only("Deve enviar dinheiro com sucesso", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("[name='username']").type("Heath93");
+    cy.get("[name='password']").type("s3cret");
+    cy.get("[type='submit']").click();
+    cy.get(".MuiButton-colorInherit").click();
+    cy.get("[data-test='user-list-search-input']").type("Lia Rosenbaum");
+    cy.contains(".MuiListItemText-root > .MuiTypography-body1", "Lia Rosenbaum").click();
+    cy.get("[name='amount']").type(0.750);
+    cy.get("[placeholder='Add a note']").type("Payment of Alimony");
+    cy.get("[data-test='transaction-create-submit-payment']").click();
+
+    // Verifica se a transação foi concluída com sucesso
+    cy.get(".MuiAlert-root").should("be.visible").and("contain", "Transaction submitted");
+  });
+});
+
+describe("Enviar dinheiro com saldo insuficiente", () => {
+  it.skip("Deve exibir mensagem de erro ao enviar dinheiro sem saldo suficiente", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("[name='username']").type("Dina20");
+    cy.get("[name='password']").type("s3cret");
+    cy.get("[type='submit']").click();
+    cy.get(".MuiButton-colorInherit").click();
+    cy.get("#user-list-search-input").type("Ted Parisian");
+    cy.contains(".MuiListItemText-root > .MuiTypography-body1", "Ted Parisian").click();
+    cy.get("[name='amount']").type(2000);
+    cy.get("[placeholder='Add a note']").type("Legal expenses and costs");
+    cy.get("[data-test='transaction-create-submit-payment']").click();
+
+    // Verifica se uma mensagem de erro foi exibida
+    cy.get(".error-message").should("be.visible").and("contain", "Saldo insuficiente");
+
+    // Confirma que o saldo não foi alterado
+    cy.get(".balance").should("not.contain", "Saldo atualizado");
+
+    // Assert: Confirma que a transação não foi registrada
+    cy.get(".transaction-list").should("not.contain", "Legal expenses and costs");
   });
 });
